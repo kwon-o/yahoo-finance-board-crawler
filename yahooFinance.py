0a65f92732fa8cb5e2c4f8381555339140ef2e12
kwon-o <koj1228853@gmail.com>
2021-02-07 20:57:02 +0900

    Functionalization

--- yahooFinance.py
+++ yahooFinance.py
@@ -1,44 +1,50 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
 import time
 import datetime
 import pandas as pd
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
-now = datetime.datetime.now().strftime("%Y%m%d")
+def yahooFinanceCrawler(URL):
+    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver.get(url=URL)
 
-URL = 'https://finance.yahoo.co.jp/cm/message/1998407/ffc7pjbf6q3t2a'
+    SCROLL_PAUSE_SEC = 3
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
-driver = webdriver.Chrome(executable_path='chromedriver')
-driver.get(url=URL)
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(SCROLL_PAUSE_SEC)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
-SCROLL_PAUSE_SEC = 3
-last_height = driver.execute_script("return document.body.scrollHeight")
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    hotKeys = soup.find_all("div", class_="comment")
 
-while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-    time.sleep(SCROLL_PAUSE_SEC)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+    result = []
+    cnt = 0
 
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-hotKeys = soup.find_all("div", class_="comment")
+    hotKeys.reverse()
+    for key in hotKeys:
+        try:
+            comTime = key.find("p", class_="comWriter").text.split('\n')[2]
+        except AttributeError:
+            continue
+        comText = key.find("p", class_="comText").text
+        cnt += 1
+        row = [cnt, comTime, comText]
+        result.append(row)
 
-result = []
-cnt = 0
-csv_name = now+"_com.csv"
-hotKeys.reverse()
-for key in hotKeys:
-    try:
-        comTime = key.find("p", class_="comWriter").text.split('\n')[2]
-    except AttributeError :
-        continue
-    comText = key.find("p", class_="comText").text
-    cnt += 1
-    row = [cnt, comTime, comText]
-    result.append(row)
+    saveCsv(result)
 
-result = pd.DataFrame(result, columns=['index', 'time', 'text'])
-result.to_csv(csv_name, index=False, encoding='utf-8')
+def saveCsv(result):
+    now = datetime.datetime.now().strftime("%Y%m%d")
+    csv_name = now + "_com.csv"
+    result = pd.DataFrame(result, columns=['index', 'time', 'text'])
+    result.to_csv(csv_name, index=False, encoding='utf-8')
+
+if __name__ == '__main__':
+    URL = 'https://finance.yahoo.co.jp/cm/message/1998407/ffc7pjbf6q3t2a'
+    yahooFinanceCrawler(URL)
